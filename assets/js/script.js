4b7191a15a8a8d1d9aba15c476dc7b55eb6505f8
Shamsddine <goumdissshamsddine@gmail.com>
2023-10-23 10:38:53 +0200

    homme douze

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,14 @@ class information {
 }
 // 73 97
 let men1 = new information ('73',);
-let men2
+let men2 = new information ('97',);
+let men3 = new information ('73');
+let men4 = new information ('73');
+let men5 = new information ('73');
+let men6 = new information ('73');
+let men7 = new information ('73');
+let men8 = new information ('73');
+let men9 = new information ('73');
+let men10 = new information ('73');
+let men11= new information ('73');
+let men12= new information ('73');
