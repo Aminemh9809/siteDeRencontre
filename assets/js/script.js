f7dc59b4639be03c1dacca2f2655d192438509d9
Shamsddine <goumdissshamsddine@gmail.com>
2023-10-23 11:15:33 +0200

    search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 class information {
-    constructor(id,photo,gender,age,department,interest,description){
+    constructor(id,photo,gender,age,department,interest,description, search,){
         this.id = id;
         this.photo = photo;
         this.gender = gender;
@@ -7,11 +7,12 @@ class information {
         this.department = department;
         this.interest = interest;
         this.description = description;
+        this.search = 0;
     }
 }
 // 73 97
-let men1 = new information ('73', "men1.jpg", "male", "19", "Informatique, manipulation de masse, le mensonge et les caméras et mes cours de français", "je suis en étudiant");
-let men2 = new information ('97', "men2.jpg", "male", "33", "Le cinéma et les chats", "chef restaurateur et cuisinier dans un restaurant 2 etoiles",);
+let men1 = new information ('73', "men1.jpg", "male", "19", "Informatique, manipulation de masse, le mensonge et les caméras et mes cours de français", "je suis en étudiant", "Je cherche une femme",);
+let men2 = new information ('97', "men2.jpg", "male", "33", "Le cinéma et les chats", "chef restaurateur et cuisinier dans un restaurant 2 etoiles","je cherche une femme");
 let men3 = new information ('73', "men3.jpg", "male", "25", "Warhammer", "je suis en étudiant en medecine");
 let men4 = new information ('73', );
 let men5 = new information ('73');
