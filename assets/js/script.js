1bb6acf8b016501b06d079d6db0cda4510dbf2af
Amine Mh <mahdiamine@outlook.com>
2023-10-23 10:31:26 +0200

    second

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-class information ();
+class information {
+    constructor(id,photo,gender,age,department,description){
+        this.id = id;
+        this.photo = photo;
+        this.gender = gender;
+        this.age = age;
+        this.department = department;
+        this.description = description;
+    }
+}
+// 73 96
+let men
