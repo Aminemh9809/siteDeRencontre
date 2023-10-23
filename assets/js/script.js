3b870bb0f39c540b5d2a6b38ee3371ce96ece741
Amine Mh <mahdiamine@outlook.com>
2023-10-23 10:37:15 +0200

    go ahead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 class information {
-    constructor(id,photo,gender,age,department,description){
+    constructor(id,photo,gender,age,department,interest,description){
         this.id = id;
         this.photo = photo;
         this.gender = gender;
         this.age = age;
         this.department = department;
+        this.interest = interest;
         this.description = description;
     }
 }
-// 73 96
-let men
+// 73 97
+let men1 = new information ('73',);
+let men2
